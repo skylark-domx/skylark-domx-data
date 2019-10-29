e3184a4e7013858e3463b1c50d449e4678f48c94
hudaokeji <github@hudaokeji.com>
2019-10-30 07:17:42 +0900

    support velm and query

--- dist/skylark-domx-data/main.js
+++ dist/skylark-domx-data/main.js
@@ -5,5 +5,5 @@
  * @link www.skylarkjs.org
  * @license MIT
  */
-define(["./data"],function(n){return n});
+define(["./data","skylark-domx-velm","skylark-domx-query"],function(a,t,r){return t.delegate(["attr","data","prop","removeAttr","removeData","text","val"],datax),r.fn.text=r.wraps.wrapper_value(datax.text,datax,datax.text),r.fn.attr=r.wraps.wrapper_name_value(datax.attr,datax,datax.attr),r.fn.removeAttr=r.wraps.wrapper_every_act(datax.removeAttr,datax),r.fn.prop=r.wraps.wrapper_name_value(datax.prop,datax,datax.prop),r.fn.removeProp=r.wraps.wrapper_every_act(datax.removeProp,datax),r.fn.data=r.wraps.wrapper_name_value(datax.data,datax,datax.data),r.fn.removeData=r.wraps.wrapper_every_act(datax.removeData,datax),r.fn.val=r.wraps.wrapper_value(datax.val,datax,datax.val),a});
 //# sourceMappingURL=sourcemaps/main.js.map
--- dist/uncompressed/skylark-domx-data/main.js
+++ dist/uncompressed/skylark-domx-data/main.js
@@ -1,5 +1,35 @@
 define([
-	"./data"
-],function(data){
+	"./data",
+	"skylark-domx-velm",
+	"skylark-domx-query"	
+],function(data,velm,$){
+    // from ./datax
+    velm.delegate([
+        "attr",
+        "data",
+        "prop",
+        "removeAttr",
+        "removeData",
+        "text",
+        "val"
+    ], datax);
+
+    $.fn.text = $.wraps.wrapper_value(datax.text, datax, datax.text);
+
+    $.fn.attr = $.wraps.wrapper_name_value(datax.attr, datax, datax.attr);
+
+    $.fn.removeAttr = $.wraps.wrapper_every_act(datax.removeAttr, datax);
+
+    $.fn.prop = $.wraps.wrapper_name_value(datax.prop, datax, datax.prop);
+
+    $.fn.removeProp = $.wraps.wrapper_every_act(datax.removeProp, datax);
+
+    $.fn.data = $.wraps.wrapper_name_value(datax.data, datax, datax.data);
+
+    $.fn.removeData = $.wraps.wrapper_every_act(datax.removeData, datax);
+
+    $.fn.val = $.wraps.wrapper_value(datax.val, datax, datax.val);
+
+
 	return data;
 });
